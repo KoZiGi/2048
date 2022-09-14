ceaad7b7bd612892c5af877abdd9220712aff553
zickbalazs <balazs.zick2@gmail.com>
2022-09-14 08:11:11 +0200

    Done #1

--- 2048-WPF/Form1.Designer.cs
+++ 2048-WPF/Form1.Designer.cs
@@ -29,13 +29,240 @@ namespace _2048_WPF
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.UpBtn = new System.Windows.Forms.Button();
+            this.SwitchToKbdBtn = new System.Windows.Forms.Button();
+            this.DownBtn = new System.Windows.Forms.Button();
+            this.RightBtn = new System.Windows.Forms.Button();
+            this.LeftBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(50, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(68, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(50, 50);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(68, 68);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(50, 50);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(12, 68);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(50, 50);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(124, 68);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(50, 50);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(124, 12);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(50, 50);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(180, 12);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(50, 50);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(180, 68);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(50, 50);
+            this.panel8.TabIndex = 0;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(12, 124);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(50, 50);
+            this.panel9.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(68, 124);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(50, 50);
+            this.panel10.TabIndex = 0;
+            // 
+            // panel11
+            // 
+            this.panel11.Location = new System.Drawing.Point(68, 180);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(50, 50);
+            this.panel11.TabIndex = 0;
+            // 
+            // panel12
+            // 
+            this.panel12.Location = new System.Drawing.Point(12, 180);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(50, 50);
+            this.panel12.TabIndex = 0;
+            // 
+            // panel13
+            // 
+            this.panel13.Location = new System.Drawing.Point(124, 180);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(50, 50);
+            this.panel13.TabIndex = 0;
+            // 
+            // panel14
+            // 
+            this.panel14.Location = new System.Drawing.Point(124, 124);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(50, 50);
+            this.panel14.TabIndex = 0;
+            // 
+            // panel15
+            // 
+            this.panel15.Location = new System.Drawing.Point(180, 124);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(50, 50);
+            this.panel15.TabIndex = 0;
+            // 
+            // panel16
+            // 
+            this.panel16.Location = new System.Drawing.Point(180, 180);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(50, 50);
+            this.panel16.TabIndex = 0;
+            // 
+            // UpBtn
+            // 
+            this.UpBtn.Location = new System.Drawing.Point(101, 236);
+            this.UpBtn.Name = "UpBtn";
+            this.UpBtn.Size = new System.Drawing.Size(40, 40);
+            this.UpBtn.TabIndex = 1;
+            this.UpBtn.Text = "Fel";
+            this.UpBtn.UseVisualStyleBackColor = true;
+            // 
+            // SwitchToKbdBtn
+            // 
+            this.SwitchToKbdBtn.Location = new System.Drawing.Point(101, 282);
+            this.SwitchToKbdBtn.Name = "SwitchToKbdBtn";
+            this.SwitchToKbdBtn.Size = new System.Drawing.Size(40, 40);
+            this.SwitchToKbdBtn.TabIndex = 1;
+            this.SwitchToKbdBtn.UseVisualStyleBackColor = true;
+            // 
+            // DownBtn
+            // 
+            this.DownBtn.Location = new System.Drawing.Point(101, 328);
+            this.DownBtn.Name = "DownBtn";
+            this.DownBtn.Size = new System.Drawing.Size(40, 40);
+            this.DownBtn.TabIndex = 1;
+            this.DownBtn.Text = "Le";
+            this.DownBtn.UseVisualStyleBackColor = true;
+            // 
+            // RightBtn
+            // 
+            this.RightBtn.Location = new System.Drawing.Point(147, 282);
+            this.RightBtn.Name = "RightBtn";
+            this.RightBtn.Size = new System.Drawing.Size(40, 40);
+            this.RightBtn.TabIndex = 1;
+            this.RightBtn.Text = "Jobb";
+            this.RightBtn.UseVisualStyleBackColor = true;
+            // 
+            // LeftBtn
+            // 
+            this.LeftBtn.Location = new System.Drawing.Point(55, 282);
+            this.LeftBtn.Name = "LeftBtn";
+            this.LeftBtn.Size = new System.Drawing.Size(40, 40);
+            this.LeftBtn.TabIndex = 1;
+            this.LeftBtn.Text = "Bal";
+            this.LeftBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(246, 442);
+            this.Controls.Add(this.LeftBtn);
+            this.Controls.Add(this.RightBtn);
+            this.Controls.Add(this.DownBtn);
+            this.Controls.Add(this.SwitchToKbdBtn);
+            this.Controls.Add(this.UpBtn);
+            this.Controls.Add(this.panel16);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Button UpBtn;
+        private System.Windows.Forms.Button SwitchToKbdBtn;
+        private System.Windows.Forms.Button DownBtn;
+        private System.Windows.Forms.Button RightBtn;
+        private System.Windows.Forms.Button LeftBtn;
     }
 }
 
