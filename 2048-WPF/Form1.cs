636ecaa62c3fe480b32037d658495f83188992ea
zickbalazs <balazs.zick2@gmail.com>
2022-09-14 09:36:06 +0200

    HighScore #3 done körönkénti randomizálás kész

--- 2048-WPF/Form1.cs
+++ 2048-WPF/Form1.cs
@@ -7,24 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _2048_WPF
 {
     public partial class Form1 : Form
     {
-        public int[,] GameField = new int[4, 4];
+        public int[,] GameField = new int[6, 6];
         bool WASD = false;
+        public int moves = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-
+        private void CreateHighScores()
+        {
+            HighScoreDialog hs = new HighScoreDialog(moves);
+            hs.Show();
+        }
         private void DoMove(int direction)
         {
 
         }
-
+        private int[,] Randomize()
+        {
+            int[,] tempfield = GameField;
+            Random x = new Random(), y = new Random();
+            int X = x.Next(1, 5), Y = y.Next(1, 5);
+            if (tempfield[X, Y] == 0)
+            {
+                tempfield[X, Y] = x.Next(0, 1) == 0 ? 2 : 4;
+                return tempfield;
+            }
+            else return Randomize();
+        } 
         private void UpBtn_Click(object sender, EventArgs e)
         {
             DoMove(0);
